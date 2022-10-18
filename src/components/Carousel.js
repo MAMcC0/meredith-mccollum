f888de41f92423e7471e9e50d57b655e9b9733da
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-10-17 19:30:24 -0700

    added photos for portfolio

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -15,42 +15,43 @@ function CarouselControl(){
             <Carousel.Item>
                 <img src='#'/>
                 <Carousel.Caption>
-                    <a href="#">Portfolio1Live</a>
-                    <a href='#'>Portfolio1Repo</a>
+                    
+                    <a href="https://sensationnel-baguette-60188.herokuapp.com/login">Beginner's Luck</a>
+                    <a href='#'>Code Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img/>
                 <Carousel.Caption>
-                    <a href="#">Portfolio1Live</a>
-                    <a href='#'>Portfolio1Repo</a>
+                    <a href="https://down-with-the-fitness.herokuapp.com">Down with the Fitness</a>
+                    <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img/>
                 <Carousel.Caption>
-                    <a href="#">Portfolio1Live</a>
-                    <a href='#'>Portfolio1Repo</a>
+                    <a href="https://mamcc0.github.io/day-planner/">Day Planner</a>
+                    <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img/>
                 <Carousel.Caption>
-                    <a href="#">Portfolio1Live</a>
-                    <a href='#'>Portfolio1Repo</a>
+                    <a href="https://vast-tundra-99915.herokuapp.com/">Note Keeper</a>
+                    <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img/>
                 <Carousel.Caption>
-                    <a href="#">Portfolio1Live</a>
-                    <a href='#'>Portfolio1Repo</a>
+                    <a href="https://mamcc0.github.io/weather-dashboard/">Weather Application</a>
+                    <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img/>
                 <Carousel.Caption>
-                    <a href="#">Portfolio1Live</a>
+                    <a href="#">Bug Tracker</a>
                     <a href='#'>Portfolio1Repo</a>
                 </Carousel.Caption>
             </Carousel.Item>
