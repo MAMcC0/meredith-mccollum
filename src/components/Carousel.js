fea17cbfcd2e441c56d61cf20c0f2bd215e323f2
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-10-17 19:48:02 -0700

    renamed all project screenshots

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -22,28 +22,28 @@ function CarouselControl(){
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img/>
+                <img src="photos/down-with-fitness.png" alt="Down with the fitness landing page"/>
                 <Carousel.Caption>
                     <a href="https://down-with-the-fitness.herokuapp.com">Down with the Fitness</a>
                     <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img/>
+                <img src="../photos/day-planner.png" alt="Work Scheduler landing page"/>
                 <Carousel.Caption>
                     <a href="https://mamcc0.github.io/day-planner/">Day Planner</a>
                     <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img/>
+                <img src='../../photos/note-taker.png' alt="Screenshot of notetaker app"/>
                 <Carousel.Caption>
                     <a href="https://vast-tundra-99915.herokuapp.com/">Note Keeper</a>
                     <a href='#'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img/>
+                <img src="weather-dash.png" alt="screenshot of weather dashboard"/>
                 <Carousel.Caption>
                     <a href="https://mamcc0.github.io/weather-dashboard/">Weather Application</a>
                     <a href='#'>Repository</a>
