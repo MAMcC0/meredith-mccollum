af666a1f58123b14068baa53cd88653417667b2a
MAMcC0 <meredith.a.mccollum@gmail.com>
2022-11-01 12:51:37 -0700

    fixed dead links

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -24,42 +24,42 @@ function CarouselControl(){
                 <Carousel.Caption>
                     
                     <p><a href="https://sensationnel-baguette-60188.herokuapp.com/login">Beginner's Luck</a></p>
-                    <a href='#'>Code Repository</a>
+                    <a href='https://github.com/MAMcC0/poker-site'>Code Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img className="circle" src={Carousel4} alt="Down with the fitness landing page"/>
                 <Carousel.Caption>
                     <p><a href="https://down-with-the-fitness.herokuapp.com">Down with the Fitness</a></p>
-                    <a href='#'>Repository</a>
+                    <a href='https://github.com/MAMcC0/down-with-the-fitness'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img className="circle" src={Carousel2} alt="Work Scheduler landing page"/>
                 <Carousel.Caption>
                     <p><a href="https://mamcc0.github.io/day-planner/">Day Planner</a></p>
-                    <a href='#'>Repository</a>
+                    <a href='https://github.com/MAMcC0/day-planner'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img className="circle" src={Carousel3} alt="Screenshot of notetaker app"/>
                 <Carousel.Caption>
                     <p><a href="https://vast-tundra-99915.herokuapp.com/">Note Keeper</a></p>
-                    <a href='#'>Repository</a>
+                    <a href='https://github.com/MAMcC0/note-keeper'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img className="circle" src={Carousel5} alt="screenshot of weather dashboard"/>
                 <Carousel.Caption>
                     <p><a href="https://mamcc0.github.io/weather-dashboard/">Weather Application</a></p>
-                    <a href='#'>Repository</a>
+                    <a href='https://github.com/MAMcC0/weather-dashboard'>Repository</a>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img className="circle" src={Carousel6} alt="Screenshot of Not American Football site home"/>
                 <Carousel.Caption>
-                    <p><a href="#">Not American Football</a></p>
-                    <a href='#'>Portfolio1Repo</a>
+                    <p><a href="https://kevinfassu.github.io/notamericanfootball/">Not American Football</a></p>
+                    <a href='https://github.com/kevinfassu/notamericanfootball'>Portfolio1Repo</a>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
